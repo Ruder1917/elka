09e78b2820bea8d4e5f8478164666b0b985ae50a
Gusen Abdulatipov <132754718+Ruder1917@users.noreply.github.com>
2023-09-19 22:03:04 +0300

    elka
    Ёлка на Си Шрек при помощи звёздочек

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                // Выводим пробелы перед рисунком
+                for (int j = 0; j < height - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Выводим звездочки для рисунка
+                for (int k = 0; k < i * 2 + 1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
